be571ed81264ae591aea9c0d6f5d7ab77a581176
OmkarThote <thoteomkar72@gmail.com>
2025-08-08 22:21:01 +0530

    27441

--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+
+    def _str_(self):
+        return self.name
