1ec551844ba1ed59b339b07040ba43478473c2fb
OmkarThote <thoteomkar72@gmail.com>
2025-08-10 22:21:07 +0530

    27442

--- account/views.py
+++ account/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+def home():
+    return()'''
+    <h1>Welcome to Our Restaurant</h1>
+    <p><a href="/about">About Us</a></p>
+    '''
+
+    def about():
+        return'''
+        <h1>About Us</h1>
+        <p>Welcome to Gourmet Haven - where taste meets tradition! We serve freshly
+        prepared dishes made from the finest ingredients, ensuring every bite is a delightful experience.</p>
+        <img src="https://via.placeholder.com/400*250
+        alt="Restaurant Image" />
+        <p><a href="/">Back to Home</a></p>
+
+    if __name__== '__main__':
+        app.run(debug=True)
