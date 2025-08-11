739c548576ae7356025592902c55229eafbe389e
OmkarThote <thoteomkar72@gmail.com>
2025-08-11 21:46:41 +0530

    29316

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.conf import settings
 # Create your views here.
+
+def home(request):
+    context = {
+        'restaurant_name': 'Delicious_Bites',
+        'welcome_message': 'welcome to Delicious Bites! Enjoy our tasty menu.',
+        'phone_number':
+    settings.RESTAURANT_PHONE_NUMBER
+    }
+return render(request, 'home.html', context)
