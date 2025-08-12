bb91fde35e14e8188612bc79915cb4839127518f
OmkarThote <thoteomkar72@gmail.com>
2025-08-12 22:21:07 +0530

    29314

--- home/migrations/__init__.py
+++ home/migrations/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template_string
 app = Flask(__name__)
 
 # HTML with CSS styling embedded
-HTML_PAGE = ""
+HTML_PAGE = """
 <!DOCTYPE html>
 <html>
 <head>
@@ -15,3 +15,28 @@ HTML_PAGE = ""
             margin: 0;
             padding: 20px;
         }
+        h1 {
+            color: #333;
+            text-align: center;
+        }
+        p {
+            font-size: 18px;
+            color: #555;
+            line-height: 1.6;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h1>Welcome to My Homepage</h1>
+    <p>This is a simple page with basic CSS styling.</p>
+</body>
+</html>
+"""
+
+@app.route("/")
+def home():
+    return render_template_string(HTML_PAGE)
+
+if __name__ == "__main__":
+    app.run(debug=True)
