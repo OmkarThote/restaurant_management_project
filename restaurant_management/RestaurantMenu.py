0a5fbe07450b942ee08fb47065c9f08577353310
OmkarThote <thoteomkar72@gmail.com>
2025-08-17 22:49:08 +0530

    34252

---
+++ restaurant_management/RestaurantMenu.py
@@ -0,0 +1,24 @@
+# views.py
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def get_menu(request):
+    menu = [
+        {
+            "name": "Margherita Pizza",
+            "description": "Classic pizza with tomato sauce, mozzarella, and cheese", 
+            "price": 8.99
+        },
+        {
+            "name": "Veggie Burger",
+            "description": "Grilled veggie patty with lettuce, tomato, and cheese",
+            "price": 7.49
+        },
+        {
+            "name": "Pasta Alfredo",
+            "description": "Pasta in creamy Alfredo sauce with mushrooms",
+            "price": 10.50
+        }
+    ]
+    return Response(menu)
