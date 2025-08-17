acd11aa3c85c9e32335b4a3dd602aae6b973454a
OmkarThote <thoteomkar72@gmail.com>
2025-08-17 23:13:40 +0530

    34251

--- orders/admin.py
+++ orders/admin.py
@@ -1,2 +1,14 @@
 from django.contrib import admin
+from .models import Menu, Order
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "description")
+    search_fields = ("name",)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("customer_name", "menu_item", "quantity", "created_at")
+    list_filter = ("created_at",)
+    search_fields = ("customer_name",)
 
