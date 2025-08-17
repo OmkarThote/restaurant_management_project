36f4a9c5699a04d16403bab010ed1e6a7ff738bd
OmkarThote <thoteomkar72@gmail.com>
2025-08-17 23:07:18 +0530

    34251

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Menu(models.Model)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    customer_name = models.CharField(max_length=100)
+    menu_item = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order #{self.id} - {self.customer_name}"
