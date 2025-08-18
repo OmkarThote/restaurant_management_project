77a682637c843153903b712d4d0f0e9b9e8e1142
OmkarThote <thoteomkar72@gmail.com>
2025-08-18 22:45:09 +0530

    34249

--- home/views.py
+++ home/views.py
@@ -1,29 +1,6 @@
-from django.shortcuts import render
+from django.urls import path
+from . import views
 
-# Create your views here.
-
-from .models import Restaurant
-
-def home(request):
-    restaurant = Restaurant.objects.first()
-
-# get first Restaurant
-
-return render(request, 'home.html',
-{
-    'restaurant_name': restaurant.name if restaurant else 'No Name'
-})
-
-def home(request):
-    return render(request, 'home_html')
-
-
-
-def custom_404(request, exception):
-    return render(request, '404.html', status=404)
-
-
-
-def home(request):
-    return render(request, 'home.html')
-    
+urlpatterns = [
+    path("", views.home, name="home"),
+]
